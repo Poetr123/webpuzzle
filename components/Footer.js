5ce519d3037e17d28259d180dff45a185bb51752
Poetr123 <puput.tampan1234@gmail.com>
2025-12-10 08:58:11 +0700

    Ganti footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer(){
   return (
     <footer style={{padding:20, borderTop:'1px solid #eee', marginTop:40, textAlign:'center', fontSize:13, color:'#666'}}>
-      © Bank Fiktif — layanan simulasi dan forensik digital
+      © Bank Fiktif 2018 - 2026
     </footer>
   )
 }
